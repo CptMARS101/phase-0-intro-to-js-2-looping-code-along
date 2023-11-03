3d88841a56355c14058c5c90184a2637c78b9c8f
Marcus Kitchen <marcusdkitchen@gmail.com>
2023-11-03 03:17:59 -0400

    LoopingCODEALONG

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+        return gifts;
+    }
+    let name = ["Guadalupe", "Ollie", "Aki"];
+    let eventName = "surprise";
+    
+    function writeCards(name, eventName) {
+      let message = [];
+      for (let i = 0; i < name.length; i++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`);
+      }
+      return message;
+    }
+function countDown(num) {
+    if(!Number.isInteger(num)) {
+        console.log(10);
+        return;
+    } 
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
